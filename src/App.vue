e5a27b9635c869b2458eb0ef4573b8f456b16d52
Dara218 <ralphzacbautista@gmail.com>
2025-08-15 22:51:25 +0800

    MICRO-BLOG-1 - Setup Vue JS

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  
+</script>
+
+<template>
+  <header>
+    <!-- Input header here. -->
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia maxime optio voluptas reiciendis! Unde consectetur aspernatur repellendus dolore necessitatibus adipisci amet suscipit quis eius. Dolore accusantium ratione reprehenderit ab eius.
+  </div>
+</template>
+
+<script setup>
+
+</script>
